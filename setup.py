8e5afdcb349c73c1fd0eb2ed499a75ade7870529
Barcelona16 <wqw16@mails.tsinghua.edu.cn>
2019-08-31 13:30:02 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+def getTxt():
+        txt = open("requirements.txt","r").read()#读取需要安装的第三方库
+        return txt
+kusTxt=getTxt()
+words=kusTxt.split('\n')
+for word in words:
+    os.system("pip install -i https://pypi.tuna.tsinghua.edu.cn/simple"+word)#执行cmd命令
+    print("{}成功安装".format(word))
